8f95369c606ecfaea7d66cfc8dda5e90b761f370
mst0xsd <mst0xsd@gmail.com>
2017-02-02 17:41:40 +0800

    modify

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Excalibur on 2/2/17.
+ */
+$(document).ready(function () {
+    var WinHeight = $('.billboard').height() - 100;
+    var BgColor = 'rgba(255, 255, 255, .1)';
+    var DarkBgColor = 'rgba(238, 169, 89, .9)';
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop < WinHeight) {
+            $('header').css("background-color", BgColor);
+        } else {
+            $('header').css("background-color", DarkBgColor);
+        }
+    })
+});
